5d53ff1f55af4da8c7ca5108952561fa96ee33f7
Stephen <stevriss22@gmail.com>
2021-11-22 17:19:45 -0500

    commit tree 1fa7ae97508fd19c3bc12ec96250615b03720ab9

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,9 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  def request_info
+    student = Student.find(params[:id])
+    render json: student
+  end
 
 end
